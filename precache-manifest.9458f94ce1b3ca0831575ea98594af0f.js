d198628c3034285a1fc495e1235d8734b80d96be
vanessamontesb <vanessamontesb@gmail.com>
2019-08-15 08:32:55 -0500

    Updates

---
+++ precache-manifest.9458f94ce1b3ca0831575ea98594af0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8e53b4954ee72ff194343217b22bb9",
+    "url": "/Libeus/index.html"
+  },
+  {
+    "revision": "1d5f8ad1d75e37cec669",
+    "url": "/Libeus/static/css/main.9052b6cc.chunk.css"
+  },
+  {
+    "revision": "62eec6e05756576b34e9",
+    "url": "/Libeus/static/js/2.a25ed162.chunk.js"
+  },
+  {
+    "revision": "1d5f8ad1d75e37cec669",
+    "url": "/Libeus/static/js/main.ec1c9804.chunk.js"
+  },
+  {
+    "revision": "17bcc00d4ae7275e0f86",
+    "url": "/Libeus/static/js/runtime~main.fc116def.js"
+  }
+]);
